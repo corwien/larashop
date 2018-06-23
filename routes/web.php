8582b1b543a8aa1a767759eed138e8225a218447
Corwien <corwien@126.com>
2018-06-23 23:35:41 +0800

    邮箱验证中间件

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,22 @@ Route::get('/', function () {
 });
 */
 
+// Route::get('/home', 'HomeController@index')->name('home');
+
 Route::get('/', 'PagesController@root')->name('root');
 
 Auth::routes();
 
-// Route::get('/home', 'HomeController@index')->name('home');
+Route::group(['middleware' => 'auth'], function() {
+
+    // 需要把这个路由放在 auth 这个中间件的路由组里面，因为只有已经登录的用户才能看到这个提示界面。
+    Route::get('/email_verify_notice', 'PagesController@emailVerifyNotice')->name('email_verify_notice');
+
+    // 开始
+    Route::group(['middleware' => 'email_verified'], function() {
+        Route::get('/test', function() {
+            return 'Your email is verified';
+        });
+    });
+    // 结束
+});
