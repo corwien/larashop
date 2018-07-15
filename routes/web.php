4a7a01f7a9fe1bc93c3c40b0a5ae2d1f4cf4b3f6
Corwien <corwien@126.com>
2018-07-15 10:19:03 +0800

    用户界面 - 订单列表页

--- routes/web.php
+++ routes/web.php
@@ -57,14 +57,19 @@ Route::group(['middleware' => 'auth'], function() {
         Route::get('cart', 'CartController@index')->name('cart.index');
         Route::delete('cart/{sku}', 'CartController@remove')->name('cart.remove');
 
-        /*
-        Route::get('/test', function() {
-            return 'Your email is verified';
-        });
-        */
-
         //
         Route::post('orders', 'OrdersController@store')->name('orders.store');
+        Route::get('orders', 'OrdersController@index')->name('orders.index');
+
+
+
+
+
+        /*
+       Route::get('/test', function() {
+           return 'Your email is verified';
+       });
+       */
     });
     // 结束
 });
